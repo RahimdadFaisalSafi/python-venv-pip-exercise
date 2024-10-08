18338c3efec8329daaa47154e28038d3b30807e1
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2024-10-08 16:19:27 +0200

    Aufgabe 1: abgeschlossen

---
+++ visualization.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+# Zahlen von 0 bis 9
+myList = list(range(10))
+
+# Berechnung der Quadratzahlen
+quadratzahlen = [i**2 for i in myList]
+
+# Berechnung der Kubikzahlen
+kubikzahlen = [i**3 for i in myList]
+
+# Diagramm für Quadratzahlen
+plt.plot(myList, quadratzahlen, label='Quadratzahlen', color='blue', marker='^')
+
+# Diagramm für Kubikzahlen
+plt.plot(myList, kubikzahlen, label='Kubikzahlen', color='red', marker='D')
+
+# Diagramm einrichten und konfigurieren
+plt.title('Quadratzahlen und Kubikzahlen von 0 bis 9')
+plt.xlabel('Zahl')
+plt.ylabel('Wert')
+
+# Legende anzeigen
+plt.legend()
+
+# Diagramm anzeigen
+plt.show()
