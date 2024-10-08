077288fa568f02d4b19601ad7f681699514f6b88
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2024-10-08 16:41:47 +0200

    Aufgabe 3: abgeschlossen

--- visualization.py
+++ visualization.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Zahlen von 0 bis 9
 myList = list(range(10))
@@ -8,6 +9,15 @@ quadratzahlen = [i**2 for i in myList]
 
 # Berechnung der Kubikzahlen
 kubikzahlen = [i**3 for i in myList]
+
+#Erstelle ein DataFrame
+myDict = {'Zahlen': myList, 'Quadraten': quadratzahlen, 'Kubikzahlen':kubikzahlen}
+
+# Erstelle ein DataFrame mit den Zahlen 0 bis 9, deren Quadraten und deren Kubikzahlen.
+df = pd.DataFrame(data=myDict)
+
+# Speichere diese Daten in CSV-Datei namens zahlen.csv.
+df.to_csv('zahlen.csv')
 
 # Diagramm für Quadratzahlen
 plt.plot(myList, quadratzahlen, label='Quadratzahlen', color='blue', marker='^')
